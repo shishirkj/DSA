027cd0c8621f244e3b29ab56b77f1e7208d8791d
Shishir <90249481+shishirkj@users.noreply.github.com>
2023-01-01 14:45:52 +0530

    Update stringpalindrome.cpp

--- strings/stringpalindrome.cpp
+++ strings/stringpalindrome.cpp
@@ -1,8 +1,25 @@
+/*
+What is palindrome ?
+A word, phrase, or sequence,or number that reads the same backwards as forwards.   Example -> madam,racecar,noon.
+
+Question -
+Check if the given string is palindrome or not 
+
+Example
+racecar ----> Yes
+fighting ---->No
+noon ------>Yes
+
+*/
+
+
+//CODE
+
 #include<string>
 #include<iostream>
 #include<vector>
 using namespace std;
-//function to 
+//function 
 class Solution {
 public:
  void StringPalindrome(vector<char>& s){
