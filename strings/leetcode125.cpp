5b7398c9fa90475cfd6acbdd3894fecb4672d1b2
Shishir <90249481+shishirkj@users.noreply.github.com>
2023-01-01 14:36:15 +0530

    Update leetcode125.cpp

--- strings/leetcode125.cpp
+++ strings/leetcode125.cpp
@@ -1,3 +1,21 @@
+/*
+QUESTION -
+A phrase is a palindrome if, after converting all uppercase letters
+into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+EXAMPLE -
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+
+*/
+
+
+
+
+// CODE
 #include<string>
 #include<iostream>
 #include<vector>
