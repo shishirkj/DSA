7b6a1a6ad1bfc55f412df5a4411d453b6595b7f1
Shishir <90249481+shishirkj@users.noreply.github.com>
2023-01-14 20:25:28 +0530

    Add files via upload

---
+++ 2D Arrays/Linear_Search.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+bool Keypresent(int arr[][4],int key,int row,int column){
+            for(int row=0;row<4;row++){
+                for(int column=0;column<4;column++){
+                    if(key == arr[row][column])
+                    return 1;
+                }
+            }
+            return 0;
+}
+void indexelement(int arr[][4],int key,int row,int column){
+     for(int row=0;row<4;row++){
+        for(int column=0;column<4;column++){
+             if(key == arr[row][column])
+                    cout<<row<<" row"<<" "<<column<<" column"<<endl;
+                }
+            }
+}
+
+
+
+int main(){
+
+
+int arr[4][4];int key;
+
+cout<<"Enter the elements of matrix:"<<endl;
+for(int i=0;i<4;i++){
+    for(int j=0;j<4;j++){
+        cin>>arr[i][j];
+    }
+}
+
+
+for(int i=0;i<4;i++){
+    for(int j=0;j<4;j++){
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+}
+
+cout<<"Enter the key:"<<endl;
+cin>>key;
+
+
+if(Keypresent(arr,key,4,4)==1){
+    cout<<"Key is present"<<endl;
+     indexelement(arr,key,4,4);
+     }
+
+else{
+    cout<<"Key is not present"<<endl;
+}
+return 0;
+
+
+}
+
+
