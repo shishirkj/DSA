ac0a2fc34f0baeeb1bff50fe68e0b265dbadf55a
Shishir <90249481+shishirkj@users.noreply.github.com>
2023-02-03 12:38:29 +0530

    Add files via upload

---
+++ HashMap/Union_of_two_arrays.cpp
@@ -0,0 +1,53 @@
+/*
+Question -
+Union of two arrays
+Given two arrays a[] and b[] of size n and m respectively. The task is to find the number of elements in the union between these two arrays.
+Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in the union.
+
+Note : Elements are not necessarily distinct.
+
+Example 1:
+Input:
+5 3
+1 2 3 4 5
+1 2 3
+Output: 
+5
+Explanation: 
+1, 2, 3, 4 and 5 are the
+elements which comes in the union set
+of both arrays. So count is 5.
+
+Example 2:
+Input:
+6 2 
+85 25 1 32 54 6
+85 2 
+Output: 
+7
+Explanation: 
+85, 25, 1, 32, 54, 6, and
+2 are the elements which comes in the
+union set of both arrays. So count is 7.
+
+problem link --> https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1
+
+*/
+
+
+//CODE
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+         set<int>s;
+      for(int i=0;i<n;i++){ 
+          s.insert(a[i]);
+      }
+      for(int i=0;i<m;i++){ 
+          s.insert(b[i]);
+      }
+      return s.size();
+    }
+};
