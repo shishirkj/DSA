83c0cc4a421a7c9810ec0c6e3bf3b037b045a138
Shishir <90249481+shishirkj@users.noreply.github.com>
2023-03-08 13:58:19 +0530

    Update 12.Help_Classmates.cpp

--- Stacks/12.Help_Classmates.cpp
+++ Stacks/12.Help_Classmates.cpp
@@ -67,3 +67,26 @@ vector<int> help_classmate(vector<int> arr, int n)
        
    
     } 
+
+
+//code
+ vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        // Your code goes here
+        vector<int>ans;
+
+        stack<int> st;
+        st.push(-1);
+        
+        for(int i = n-1;i >= 0; i--) {
+            while(st.top() >= arr[i])
+                st.pop();
+                
+           ans.push_back(st.top());
+            st.push(arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+       
+   
+    } 
